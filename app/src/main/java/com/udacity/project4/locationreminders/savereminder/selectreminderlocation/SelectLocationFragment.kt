d571d200bc2392505569bff9df6d0e11520284e5
DK Tran <dtran@genetec.com>
2022-10-28 23:36:51 -0400

    - Move request permission from RemindersActivity.kt to SelectLocationFragment.kt

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,12 +1,16 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -21,6 +25,7 @@ import com.google.android.gms.tasks.CancellationTokenSource
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
+import com.udacity.project4.locationreminders.reminderslist.REQUEST_LOACTION_PERMISSION
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
@@ -45,17 +50,40 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         binding.viewModel = _viewModel
         binding.lifecycleOwner = this
 
+        requestLocationPermission()
+
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
         return binding.root
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Check if the location permission are granted and if so enable the location data layer.
+        if (requestCode == REQUEST_LOACTION_PERMISSION) {
+            if (grantResults.isNotEmpty()) {
+                when (grantResults[0]) {
+                    PackageManager.PERMISSION_GRANTED -> {
+                        Log.i("RemindersActivity", "Location permission is granted")
+                        enableLocation()
+                    }
+                    PackageManager.PERMISSION_DENIED -> {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                            AlertDialog.Builder(requireContext())
+                                .setTitle(getString(R.string.location_permission_alert_title))
+                                .setMessage(getString(R.string.location_permission_alert_msg))
+                                .setPositiveButton("Enable") { _, _ ->
+                                    requestLocationPermission()
+                                }.show()
+                        } else {
+                            findNavController().popBackStack()
+                        }
+                    }
+                    else -> requestLocationPermission()
+                }
+            }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -162,5 +190,35 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
             true
         }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        val isAccessFineLocationGranted =
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        val isAccessCoarseLocationGranted =
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        return isAccessFineLocationGranted && isAccessCoarseLocationGranted
+    }
+
+    private fun requestLocationPermission() {
+        if (isPermissionGranted()) {
+            enableLocation()
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOACTION_PERMISSION
+            )
+        }
+    }
+
+    private fun enableLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 }
